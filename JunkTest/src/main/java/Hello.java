9c4e0465c82e4a88d53ef7e0c7acd30364b33d3a
Forrest Waggoner <nutrideath@gmail.com>
2018-10-09 10:02:51 -0400

    Added a third output string

--- JunkTest/src/main/java/Hello.java
+++ JunkTest/src/main/java/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
 		System.out.println("This is a test");
+		System.out.println("Wow, what a start.");
 	}
 	
 }
