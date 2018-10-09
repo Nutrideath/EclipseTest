6648b1967061b2aebb310a62c8fa43da38e409c9
Forrest Waggoner <nutrideath@gmail.com>
2018-10-09 11:14:55 -0400

    Testing lombok

---
+++ WatsonJobTracker/src/main/java/com/dfal/jobtracker/beans/lombokTestBean.java
@@ -0,0 +1,32 @@
+package com.dfal.jobtracker.beans;
+
+import lombok.Data;
+
+@Data public class lombokTestBean {
+		
+	
+	private boolean rushJob;
+	
+	private String location;
+	
+	private String height;
+	
+	private String width;
+	
+	private String supersName;
+	
+	private String supersAddress_line1;
+	
+	private String supersAddress_line2;
+	
+	private String supersAddress_city;
+	
+	private String supersAddress_state;
+	
+	private String supersAddress_zip;
+
+
+	
+	
+	
+}
