3fdef7d1eb277d44826eb1bd4cc7168b628d68bf
Forrest Waggoner <nutrideath@gmail.com>
2018-10-17 16:48:28 -0400

    Inherit jobType (Corporate or Residential) from selected customer

--- WatsonJT/src/main/java/com/dfal/jobtracker/beans/NewJobWizard.java
+++ WatsonJT/src/main/java/com/dfal/jobtracker/beans/NewJobWizard.java
@@ -8,6 +8,7 @@ import javax.faces.bean.ViewScoped;
 import org.primefaces.event.FlowEvent;
 
 import com.dfal.jobtracker.beans.JobBean;
+import com.dfal.jobtracker.beans.DataBean;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,20 +25,38 @@ public class NewJobWizard implements Serializable {
  
 	@ManagedProperty(value="#{jobBean}")
     private JobBean jobBean = new JobBean();
+	
+	@ManagedProperty(value="#{dataBean}")
+	private DataBean dataBean = new DataBean();
+	
+	private CustomerBean selectedCB;
      
     private boolean skip;
-          
+      
+    
+    // functions
+    
     public void save() {        
         jobBean.saveToJobTable();
     }
     
+    
     public String onFlowProcess(FlowEvent event) {
+    	
+    	//need to set a a few fields on the job from the selection of the customer
+    	selectedCB = dataBean.getSelectedCustomerBean();	//get the selected customer, if there is one    	
+    	if(selectedCB != null) {
+    		jobBean.setRowKey(selectedCB.getRowKey());			//set the rowKey field on the jobBean (the customer name)
+    		jobBean.setJobType(selectedCB.getCustomerType());	//set the jobType field on the jobBean to match customer type (Corporate or Residential)
+    	}
+    	
+    	//checks for skip boolean (not currently used)
         if(skip) {
             skip = false;   //reset in case user goes back
             return "confirm";
         }
         else {
             return event.getNewStep();
-        }
+        } 
     }
 }
