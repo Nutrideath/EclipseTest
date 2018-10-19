8ea188575a63df2089eac1ca3ce3e270784bb68a
Forrest Waggoner <nutrideath@gmail.com>
2018-10-19 14:05:35 -0400

    Changed to Azure storage (from local)

--- WatsonJT/src/main/java/com/dfal/jobtracker/model/DataManagerAzure.java
+++ WatsonJT/src/main/java/com/dfal/jobtracker/model/DataManagerAzure.java
@@ -13,17 +13,17 @@ public class DataManagerAzure {
      * There are two below - one for use online at Azure, and the other for development (local, uses Azure Storage Emulator)
      */
 	//This is the actual, online Azure account
-    //public static final String storageConnectionString = "DefaultEndpointsProtocol=https;"
-    //        + "AccountName=dfalstoragetest;"
-    //        + "AccountKey=GKPPduvRKUCV8h7ik7q9BsOlf8DPS8k/6J2VFl5mrCpGOQqTC7qJ4oRaokrBlWfMPwkszTJCPaG3L+HiGJWyiQ==";
+    public static final String storageConnectionString = "DefaultEndpointsProtocol=https;"
+            + "AccountName=dfalstoragetest;"
+            + "AccountKey=GKPPduvRKUCV8h7ik7q9BsOlf8DPS8k/6J2VFl5mrCpGOQqTC7qJ4oRaokrBlWfMPwkszTJCPaG3L+HiGJWyiQ==";
     
     //This is the local Development account using the Azure Storage Emulator
-    public static final String storageConnectionString = "DefaultEndpointsProtocol=http;"
-    + "AccountName=devstoreaccount1;"
-    + "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
-    + "BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
-    + "TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
-    + "QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
+   // public static final String storageConnectionString = "DefaultEndpointsProtocol=http;"
+   // + "AccountName=devstoreaccount1;"
+   // + "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
+   // + "BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
+   // + "TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
+   // + "QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
     
   
     /**
